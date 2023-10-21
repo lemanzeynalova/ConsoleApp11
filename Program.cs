2dd006296b0311e4725ddf9674d5aad9fcaca01a
User <User@DESKTOP-Q9QV65T>
2023-10-21 23:38:09 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ConsoleApp11
+{
+    internal class Program
+    {
+        static bool SadəMiMürəkkəb(int n)
+        {
+            if (n <= 1)
+                return false;
+            if (n <= 3)
+                return true;
+            if (n % 2 == 0 || n % 3 == 0)
+                return false;
+
+            for (int i = 5; i * i <= n; i += 6)
+            {
+                if (n % i == 0 || n % (i + 2) == 0)
+                    return false;
+            }
+
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Ədədi daxil edin: ");
+            int eded = int.Parse(Console.ReadLine());
+            bool netice = SadəMiMürəkkəb(eded);
+            Console.WriteLine(netice ? "Sadə" : "Mürəkkəb");
+        }
+    }
+}
